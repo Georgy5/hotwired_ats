8fe4f9e64d752c558eedbe4bbd43617b7f3620e3
Karl Keller <kgk86@hotmail.com>
2024-02-27 22:54:41 +0100

    Configure Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+// ./app/assets/stylesheets/**/*.css 
 module.exports = {
+  mode: 'jit',
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    "./app/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+  ],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
