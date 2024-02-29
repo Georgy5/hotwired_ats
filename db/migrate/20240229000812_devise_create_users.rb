aad0097b482998ba5f5a0e083ae06f332c4a4c40
Karl Keller <kgk86@hotmail.com>
2024-02-29 01:11:21 +0100

    Update Devise user migration
    Ensure that account reference is properly added with a UUID column type.

--- db/migrate/20240229000812_devise_create_users.rb
+++ db/migrate/20240229000812_devise_create_users.rb
@@ -32,7 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.references :account
+      t.references :account, foreign_key: true, type: :uuid
       t.string :first_name
       t.string :last_name
 
