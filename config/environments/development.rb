904b725ddc8dde17cff28adca2f99ec415ccafb2
Karl Keller <kgk86@hotmail.com>
2024-03-10 00:52:16 +0100

    Update development.rb
    Add comment on `redis-session-store`,
    which is bundled in the Gemfile.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,18 @@ Rails.application.configure do
 
   # config.session_store :redis_session_store, key: "_sessions_development", compress: true, pool_size: 5, expire_after: 1.year
   config.session_store :cache_store, key: "_sessions_development", compress: true, pool_size: 5, expire_after: 1.year
+
+  # StimulusReflex does not support :cookie_store, and we recommend switching to Redis.
+  # To use `redis-session-store`, make sure to add it to your Gemfile and run `bundle install`.
+
+  # config.session_store :redis_session_store,
+  #   serializer: :json,
+  #   on_redis_down: ->(*a) { Rails.logger.error("Redis down! #{a.inspect}") },
+  #   redis: {
+  #     expire_after: 120.minutes,
+  #     key_prefix: "session:",
+  #     url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" }
+  # }
   config.action_controller.default_url_options = {host: "localhost", port: 3000}
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
