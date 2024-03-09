7aed3454793d6a2d2c8301303b347fff0ec12d3a
Karl Keller <kgk86@hotmail.com>
2024-03-10 00:54:25 +0100

    Refactor application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,7 @@ module ApplicationHelper
       'bg-blue-900 border-blue-900'
     when :success
       'bg-green-900 border-green-900'
-    when :alert
-      'bg-red-900 border-red-900'
-    when :error
+    when :alert, :error
       'bg-red-900 border-red-900'
     else
       'bg-blue-900 border-blue-900'
